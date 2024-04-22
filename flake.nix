b99ff9ba98f5918b7ecff87beb138c453a9cd641
jonwin1 <jonatan.wincent@proton.me>
2024-04-22 19:05:37 +0200

    flake.nix

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "st flake by jonwin";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, ... }@inputs:
+    utils.lib.eachSystem ["x86_64-linux"] (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+
+        overlays = [
+          (final: prev: {
+            st = prev.st.overrideAttrs (old: {
+              src = ./.;
+              buildInputs = with prev; old.buildInputs ++ [harfbuzz];
+            });
+          })
+        ];
+      };
+    in {
+      packages.default = pkgs.st;
+
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          gnumake
+          xorg.libX11
+          xorg.libXft
+          xorg.libXinerama
+          harfbuzz
+        ];
+      };
+    });
+}
